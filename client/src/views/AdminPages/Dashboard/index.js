be15e41dd963764a2d6cdef583b0d73d2b9762bb
obitrim <pevangelist516@gmail.com>
2020-12-22 14:28:09 +0000

    added jsx template for Dashboard View

--- client/src/views/AdminPages/Dashboard/index.js
+++ client/src/views/AdminPages/Dashboard/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import './Dashboard.css';
+import Dash from '../../../components/Dash';
+import BaseTable from '../../../components/BaseTable';
 
 const Index = (props) => {
   return (
-    <div>Dashboard (adding some text)</div>
+    <>
+    	<h1 className="Dashboard__Heading">Dashboard</h1>
+    	<section className="Dashboard__Section Dashboard__Dashes">
+    		<Dash title="Total Sales" value="$4,500"/>
+    		<Dash title="Total Reports" value="30"/>
+    		<Dash title="Drugs near expiry" value="$400"/>
+    		<Dash title="Drugs out of stock" value="53"/>
+    	</section>
+    	<section className="Dashboard__Section">Graph Goes Here</section>
+    	<section className="Dashboard__Section Dashboard__RecentOrders">
+    		<BaseTable/>
+    	</section>
+    </>
   )
 }
 
