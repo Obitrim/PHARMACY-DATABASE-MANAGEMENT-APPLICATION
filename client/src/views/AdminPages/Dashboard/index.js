5a862a9b96a2ec4862d8b29027432c20bab44fd9
obitrim <pevangelist516@gmail.com>
2020-12-24 10:31:14 +0000

    improved view with additional markup

--- client/src/views/AdminPages/Dashboard/index.js
+++ client/src/views/AdminPages/Dashboard/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './Dashboard.css';
 import Dash from '../../../components/Dash';
 import BaseTable from '../../../components/BaseTable';
@@ -13,9 +15,44 @@ const Index = (props) => {
     		<Dash title="Drugs near expiry" value="$400"/>
     		<Dash title="Drugs out of stock" value="53"/>
     	</section>
-    	<section className="Dashboard__Section">Graph Goes Here</section>
+    	<section className="Dashboard__Section">
+       <h2 className="Section__Heading">Sales Graph</h2> 
+       <div className="Dashboard__SalesGraph"></div>
+      </section>
     	<section className="Dashboard__Section Dashboard__RecentOrders">
-    		<BaseTable/>
+        <h2 className="Section__Heading">
+          Recent Orders 
+          <Link to="/admin/orders" className="Dashboard__SeeAll">See All</Link> 
+        </h2>
+    		<BaseTable 
+          headings={['Drug Name', 'Description', 'Date']}
+          >
+          <tr>
+              <td>Paracetamol</td>
+              <td>I feel notious whenever i take this drug</td>
+              <td>24/12/2020</td>
+          </tr>
+          <tr>
+              <td>Paracetamol</td>
+              <td>I feel notious whenever i take this drug</td>
+              <td>24/12/2020</td>
+          </tr>
+          <tr>
+              <td>Paracetamol</td>
+              <td>I feel notious whenever i take this drug</td>
+              <td>24/12/2020</td>
+          </tr>
+          <tr>
+              <td>Paracetamol</td>
+              <td>I feel notious whenever i take this drug</td>
+              <td>24/12/2020</td>
+          </tr>
+          <tr>
+              <td>Paracetamol</td>
+              <td>I feel notious whenever i take this drug</td>
+              <td>24/12/2020</td>
+          </tr>
+            </BaseTable>
     	</section>
     </>
   )
