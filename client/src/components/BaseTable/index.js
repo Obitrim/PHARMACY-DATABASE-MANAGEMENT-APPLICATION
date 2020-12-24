20222083883b6373ccfca854da2a18ce7cde1923
obitrim <pevangelist516@gmail.com>
2020-12-24 10:33:46 +0000

    created BaseTable component

--- client/src/components/BaseTable/index.js
+++ client/src/components/BaseTable/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './BaseTable.css';
 import PropTypes from 'prop-types';
 
-const Index = ({ headings, caption, children }) => {
+const Index = ({ headings, caption, children, stripped }) => {
   return (
-   <table className="BaseTable">
+   <table className="BaseTable" border={!stripped && 0}>
 		{caption && <caption className="BaseTable__Caption">{ caption }</caption>}
 		<thead className="BaseTable__Thead">
 			<tr>
@@ -20,12 +21,14 @@ const Index = ({ headings, caption, children }) => {
 Index.propTypes = {
 	headings: PropTypes.array,
 	caption: PropTypes.string,
-	children: PropTypes.node
+	children: PropTypes.node,
+	stripped: PropTypes.bool
 }
 
 Index.defaultProps = {
 	headings: ['Heading 1', 'Heading 2', 'Heading 3'],
 	caption: "",
+	stripped: true,
 	children: <tr><td>Data 1</td><td>Data 2</td><td>Data 3</td></tr>
 }
 
