1bddb34f6557a6903e7e455aa55746c32d2d9426
obitrim <pevangelist516@gmail.com>
2021-01-04 12:36:37 +0000

    created drugs initial state

--- client/src/store/state.js
+++ client/src/store/state.js
@@ -1,5 +1,63 @@
 const state = {
-	navOpen: false
+	navOpen: false,
+
+	drugs: [
+		{
+			id: '1',
+			name: 'Paracetamol',
+			FDANumber: '12342342879',
+			manufacturer: 'Lucky Herbal',
+			adminMethod: 'Oral',
+			quantity: 300,
+			expirationDate: '24/12/2021'
+		},
+		{
+			id: '2',
+			name: 'Paracetamol',
+			FDANumber: '12342342879',
+			manufacturer: 'Lucky Herbal',
+			adminMethod: 'Oral',
+			quantity: 0,
+			expirationDate: '24/12/2021'
+		},
+		{
+			id: '3',
+			name: 'Paracetamol',
+			FDANumber: '12342342879',
+			manufacturer: 'Lucky Herbal',
+			adminMethod: 'Oral',
+			quantity: 300,
+			expirationDate: '24/12/2020'
+		},
+		{
+			id: '4',
+			name: 'Paracetamol',
+			FDANumber: '12342342879',
+			manufacturer: 'Lucky Herbal',
+			adminMethod: 'Oral',
+			quantity: 0,
+			expirationDate: '24/12/2021'
+		},
+		{
+			id: '5',
+			name: 'Paracetamol',
+			FDANumber: '12342342879',
+			manufacturer: 'Lucky Herbal',
+			adminMethod: 'Oral',
+			quantity: 200,
+			expirationDate: '24/12/2021'
+		},
+		{
+			id: '6',
+			name: 'Paracetamol',
+			FDANumber: '12342342879',
+			manufacturer: 'Lucky Herbal',
+			adminMethod: 'Oral',
+			quantity: 300,
+			expirationDate: '24/12/2020'
+		},
+		
+	]
 };
 
 export default state;
