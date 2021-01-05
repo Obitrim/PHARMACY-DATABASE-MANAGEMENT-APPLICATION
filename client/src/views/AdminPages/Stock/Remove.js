32b1117abfdffe822adfe0f011920584f549f410
obitrim <pevangelist516@gmail.com>
2021-01-05 10:13:27 +0000

    refactored code to implement filter functionality

--- client/src/views/AdminPages/Stock/Remove.js
+++ client/src/views/AdminPages/Stock/Remove.js
@@ -1,183 +1,78 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 import './Remove.css';
+import { StoreContext } from '../../../store';
 import SearchBox from '../../../components/SearchBox';
 import BaseTable from '../../../components/BaseTable';
 
 const Remove = (props) => {
-	function searchHandler(){
-		console.log('Searching');
-	}
+  const [searchKey, setSearchKey] = useState("");
+  const { store } = useContext(StoreContext);
+  /**
+   *
+   * filters drugs based on searchKey
+   *
+   * @returns {Array} drugs that match searchKey
+   */
+  
+  function getFilteredDrugs() {
+    return store.drugs.filter(drug => {
+      return new RegExp(`${searchKey}`, 'i').test(drug.name) 
+             || new RegExp(`${searchKey}`, 'i').test(drug.manufacturer) 
+             || new RegExp(`${searchKey}`, 'i').test(drug.FDANumber);
+    })
+  }
+
+  let filteredDrugs = getFilteredDrugs();
+  let drugRowsTemplate = filteredDrugs.map((drug, index) => (
+    <tr key={drug.name + index}>
+      <td>{drug.name}</td>
+      <td>{drug.FDANumber}</td>
+      <td>{drug.manufacturer}</td>
+      <td>{drug.adminMethod}</td>
+      <td>{drug.quantity}</td>
+      <td>{drug.expirationDate}</td>
+      <td>
+        <a href="#" className="Drugs__ActionIcon">
+          <DeleteIcon/>
+        </a>
+      </td>
+    </tr>
+  ));
 
   return (
     <>
     	<h1 className="View__Heading">Remove record</h1>
-    	{/*Filter box*/}
+
     	<div className="RemoveDrug__Filterbox">
     		<SearchBox 
-	    		onSearch={searchHandler}
+	    		onFilterChanged={evt => setSearchKey(evt.target.value)}
 	    		placeholder="Search by name, manufacturer or FDA number"
 	    		isFilter
 	    	/>
     		<span className="Drugs__Filterbox__PageLabel">1 of 10 pages</span>
     	</div>
-    	{/*Table*/}
+
     	<section className="Drugs__TableSection">
     		<BaseTable 
           		headings={[
 	          	'Product Name', 'FDA number', 'Manufacturer', 'Administration method', 'Stock Quantity', 'Expiration Date', 'Action'
 	          	]}
           	>
-          <tr>
-              <td>Paracetamol</td>
-              <td>12342342879</td>
-              <td>Lucky Herbal</td>
-              <td>Oral</td>
-              <td>300</td>
-              <td>24/12/2020</td>
-              <td>
-              	<a href="#" className="Drugs__ActionIcon">
-              		<DeleteIcon/>
-              	</a>
-              </td>
-          </tr>
-           <tr>
-              <td>Paracetamol</td>
-              <td>12342342879</td>
-              <td>Lucky Herbal</td>
-              <td>Oral</td>
-              <td>300</td>
-              <td>24/12/2020</td>
-              <td>
-              	<a href="#" className="Drugs__ActionIcon">
-              		<DeleteIcon/>
-              	</a>
-              </td>
-          </tr>
-           <tr>
-              <td>Paracetamol</td>
-              <td>12342342879</td>
-              <td>Lucky Herbal</td>
-              <td>Oral</td>
-              <td>300</td>
-              <td>24/12/2020</td>
-              <td>
-              	<a href="#" className="Drugs__ActionIcon">
-              		<DeleteIcon/>
-              	</a>
-              </td>
-          </tr>
-           <tr>
-              <td>Paracetamol</td>
-              <td>12342342879</td>
-              <td>Lucky Herbal</td>
-              <td>Oral</td>
-              <td>300</td>
-              <td>24/12/2020</td>
-              <td>
-              	<a href="#" className="Drugs__ActionIcon">
-              		<DeleteIcon/>
-              	</a>
-              </td>
-          </tr>
-           <tr>
-              <td>Paracetamol</td>
-              <td>12342342879</td>
-              <td>Lucky Herbal</td>
-              <td>Oral</td>
-              <td>300</td>
-              <td>24/12/2020</td>
-              <td>
-              	<a href="#" className="Drugs__ActionIcon">
-              		<DeleteIcon/>
-              	</a>
-              </td>
-          </tr>
-           <tr>
-              <td>Paracetamol</td>
-              <td>12342342879</td>
-              <td>Lucky Herbal</td>
-              <td>Oral</td>
-              <td>300</td>
-              <td>24/12/2020</td>
-              <td>
-              	<a href="#" className="Drugs__ActionIcon">
-              		<DeleteIcon/>
-              	</a>
-              </td>
-          </tr>
-           <tr>
-              <td>Paracetamol</td>
-              <td>12342342879</td>
-              <td>Lucky Herbal</td>
-              <td>Oral</td>
-              <td>300</td>
-              <td>24/12/2020</td>
-              <td>
-              	<a href="#" className="Drugs__ActionIcon">
-              		<DeleteIcon/>
-              	</a>
-              </td>
-          </tr>
-           <tr>
-              <td>Paracetamol</td>
-              <td>12342342879</td>
-              <td>Lucky Herbal</td>
-              <td>Oral</td>
-              <td>300</td>
-              <td>24/12/2020</td>
-              <td>
-              	<a href="#" className="Drugs__ActionIcon">
-              		<DeleteIcon/>
-              	</a>
-              </td>
-          </tr>
-           <tr>
-              <td>Paracetamol</td>
-              <td>12342342879</td>
-              <td>Lucky Herbal</td>
-              <td>Oral</td>
-              <td>300</td>
-              <td>24/12/2020</td>
-              <td>
-              	<a href="#" className="Drugs__ActionIcon">
-              		<DeleteIcon/>
-              	</a>
-              </td>
-          </tr>
-           <tr>
-              <td>Paracetamol</td>
-              <td>12342342879</td>
-              <td>Lucky Herbal</td>
-              <td>Oral</td>
-              <td>300</td>
-              <td>24/12/2020</td>
-              <td>
-              	<a href="#" className="Drugs__ActionIcon">
-              		<DeleteIcon/>
-              	</a>
-              </td>
-          </tr>
-           <tr>
-              <td>Paracetamol</td>
-              <td>12342342879</td>
-              <td>Lucky Herbal</td>
-              <td>Oral</td>
-              <td>300</td>
-              <td>24/12/2020</td>
-              <td>
-              	<a href="#" className="Drugs__ActionIcon">
-              		<DeleteIcon/>
-              	</a>
-              </td>
-          </tr>
+            {filteredDrugs.length === 0
+              ? <>
+                  <tr>
+                    <td>No result(s) found for <q><em>{searchKey}</em></q></td>
+                  </tr>
+                </>
+              : drugRowsTemplate }
         </BaseTable>
     	</section>
-      {/*Pagination button*/}
+
       <div className="Drugs__PaginationBtns">
       	<a href="#" className="Drugs__PaginationBtn">
       		<ChevronLeftIcon/>
