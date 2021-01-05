1f477ad8b16b150895cc995fbf011422bb9d9824
obitrim <pevangelist516@gmail.com>
2021-01-05 11:42:10 +0000

    refactored template

--- client/src/views/AdminPages/Stock/Manufacturers.js
+++ client/src/views/AdminPages/Stock/Manufacturers.js
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import PlusIcon from '@material-ui/icons/Add';
 
 import './Manufacturers.css';
+import { StoreContext } from '../../../store';
 import FilterBox from '../../../components/SearchBox';
 import ManufacturerCard from '../../../components/ManufacturerCard';
 
 const Manufacturers = (props) => {
-  return (
-    <>
-    	<h1 className="View__Heading View__Heading--Manufacturer">Manufacturers</h1>
-    	<FilterBox isFilter/>
-    	<div className="Manufacturers__Row">
-    		<ManufacturerCard/>
-    		<ManufacturerCard/>
-    		<ManufacturerCard/>
-    		<ManufacturerCard/>
-    		<ManufacturerCard/>
-    		<ManufacturerCard/>
-    		<ManufacturerCard/>
-    	</div>
-    </>
-  )
+    const [filter, setFilter] = useState('');
+    const { store } = useContext(StoreContext);
+
+    function getFilteredManufacturersData() {
+        return store.manufacturers.filter(manufacturer => {
+            let { name, drug} = manufacturer;
+            let filterRegex = new RegExp(`${filter}`, 'i');
+            return filterRegex.test(name) || filterRegex.test(drug);
+        });
+    }
+
+    let filteredManufacturers = getFilteredManufacturersData();
+
+    return (
+        <>
+        	<h1 className="View__Heading View__Heading--Manufacturer">Manufacturers</h1>
+        	<FilterBox 
+                onFilterChanged={evt => setFilter(evt.target.value)} 
+                placeholder="Search by manufacturer's name or drug name"
+                isFilter
+            />
+        	<div className="Manufacturers__Row">
+                {filteredManufacturers.map((manufacturer, index) => (
+        		  <ManufacturerCard
+                    key={manufacturer.name + index}
+                    name={manufacturer.name}
+                    drug={manufacturer.drug}
+                    phone={manufacturer.phone}
+                    email={manufacturer.email}
+                    />
+                    ))
+                }
+        	</div>
+            <button type="button" className="Manufacturers__CreateNewBtn">
+                <PlusIcon/>
+                <span className="Manufacturers__CreateNewBtn__Text">Create New</span>
+            </button>
+        </>
+    )
 }
 
 export default Manufacturers;
