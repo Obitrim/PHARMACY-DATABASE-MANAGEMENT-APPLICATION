7809aae66877192dd97390c480f6b9db17015427
obitrim <pevangelist516@gmail.com>
2021-01-06 20:07:52 +0000

    setup localhost database connection and routes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
 
 // Instantiating app
 const app = express();
-
-// Global Middlewares
+/**
+ *
+ * GLOBAL MIDDLEWARES
+ *
+ */
 app.use(helmet());
-
-app.get('/', (req, res) => {
-	res.send('Hello, Server is set up');
+app.use(express.json());
+/**
+ *
+ * DATABASE CONNECTION
+ *
+ */
+mongoose.connect('mongodb://localhost/pharmacy', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
+const connection = mongoose.connection;
+connection.once('open', () => console.log('Connected to database'));
+connection.on('error', error => console.log('Error connecting to database', error));
+/**
+ *
+ * ROUTES
+ *
+ */
+app.use('/drugs', require('./routers/drugs'));
+app.use('/reports', require('./routers/reports'));
+app.use('/receipts', require('./routers/receipts'));
+app.use('/manufacturer', require('./routers/manufacturers'));
 
 app.listen(
 	process.env.PORT || 3030, 
